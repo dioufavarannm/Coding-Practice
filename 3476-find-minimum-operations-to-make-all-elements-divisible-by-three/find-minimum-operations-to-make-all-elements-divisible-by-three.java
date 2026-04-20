761f7ba9c84ad9214fbd6f558accc2f01343e56b
dioufavarannm <dioufnm963@gmail.com>
2026-04-20 11:25:49 +0530

    Time: 1 ms (25.09%) | Memory: 43.6 MB (53.50%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        for(int n : nums)
+            if(n % 3 != 0) count++;
+        return count;
+    }
+}
